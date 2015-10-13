f116c2c95d6b2fdba5e0167761015d8076c6784f
Yurii Honchar <Yurii_Honchar@epam.com>
2015-10-13 20:02:02 +0300

    Service class declared

---
+++ GameStore.BLL/Services/GameService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using GameStore.BLL.DTO;
+using GameStore.BLL.Interfaces;
+using GameStores.DAL.Concrete;
+using GameStore.DAL.Interfaces;
+
+namespace GameStore.BLL.Services
+{
+    public class GameService : IGameService
+    {
+        private IUnitOfWork database;
+
+        public GameService(IUnitOfWork database)
+        {
+            this.database = database;
+        }
+
+        public void AddGame(GameDTO game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditGame(GameDTO game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameDTO GetGameByKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GameDTO> GetAllGames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddComment(CommentDTO comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CommentDTO> GetCommentsByGameKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GameDTO> GetGamesByGenre(int genreId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GameDTO> GetGamesByPlatformTypes(IEnumerable<int> typeIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
