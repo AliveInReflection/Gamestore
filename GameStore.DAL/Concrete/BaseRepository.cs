c0ea871bc73bf516f4182de9fd2862eec4f06208
Yurii Honchar <Yurii_Honchar@epam.com>
2015-10-13 17:29:49 +0300

    Repositories and unit of work implemented

---
+++ GameStore.DAL/Concrete/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameStore.DAL.Interfaces;
+using System.Linq.Expressions;
+using GameStore.DAL.Context;
+
+namespace GameStore.DAL.Concrete
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private DataContext context;
+
+        public BaseRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Update(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> where)
+        {
+            return context.Set<TEntity>().FirstOrDefault(where);
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return context.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where)
+        {
+            return context.Set<TEntity>().Where(where).ToList();
+        }
+    }
+}
---
+++ GameStore.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Game> Games { get;}
+        IRepository<Comment> Comments { get; }
+        IRepository<Genre> Genres { get; }
+        IRepository<PlatformType> PlatformTypes { get; }
+
+        void Save();
+    }
+}
