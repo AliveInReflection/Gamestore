6cf1ecbb055e525aa9d9aabcbb263544ecdc8106
Yurii Honchar <Yurii_Honchar@epam.com>
2015-10-13 16:44:08 +0300

    Context added

---
+++ GameStore.DAL/Context/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.DAL.Context
+{
+    public class DataContext : DbContext
+    {
+        static  DataContext()
+        {
+            
+        }
+
+        public DataContext(string connectionString)
+            :base(connectionString)
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<PlatformType> PlatformTypes { get; set; }
+    }
+}
