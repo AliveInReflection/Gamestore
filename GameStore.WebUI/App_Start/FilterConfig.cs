f00f656dccc8988238f0130a1a3be1445bf8fb5e
Yurii Honchar <Yurii_Honchar@epam.com>
2015-10-19 11:46:35 +0300

    Performance loggin implemented

--- GameStore.WebUI/App_Start/FilterConfig.cs
+++ GameStore.WebUI/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using GameStore.WebUI.Filters;
 
 namespace GameStore.WebUI
 {
@@ -8,6 +9,8 @@ namespace GameStore.WebUI
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new OutputCacheAttribute() {Duration = 60});
+            filters.Add(new PerformanceLoggingAttribute());
         }
     }
 }
