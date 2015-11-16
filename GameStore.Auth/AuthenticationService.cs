5cc8f7b819c345f936ae617efa00975891c52ed5
Yurii Honchar <Yurii_Honchar@epam.com>
2015-11-16 15:40:34 +0200

    Add proj

---
+++ GameStore.Auth/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GameStore.Infrastructure.AuthInterfaces;
+using GameStore.Infrastructure.DTO;
+
+namespace GameStore.Auth
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public void Register(UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string userName, string password, bool isPersistent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
