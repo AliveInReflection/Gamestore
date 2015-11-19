f37b9e25a264f76eceb4331741d587933833e0de
Yurii Honchar <Yurii_Honchar@epam.com>
2015-11-19 20:04:28 +0200

    enable tracking some files

---
+++ GameStore.WebUI/Models/Account/ManageUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.WebUI.App_LocalResources.Localization;
+
+namespace GameStore.WebUI.Models.Account
+{
+    public class ManageUserViewModel
+    {
+        public IEnumerable<SelectListItem> RoleItems { get; set; }
+
+        public DisplayUserViewModel User { get; set; }
+
+        [Display(ResourceType = typeof(ModelRes), Name = "AccountRoles")]
+        [Required(ErrorMessageResourceType = typeof(ValidationRes), ErrorMessageResourceName = "FieldIsRequired")]
+        public IEnumerable<int> Roles { get; set; }
+
+        public IEnumerable<ClaimViewModel> Claims { get; set; }
+    }
+}
