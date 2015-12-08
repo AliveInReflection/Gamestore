55cc468efc6c68f38454cccd21d3193cffd25e4a
Yurii Honchar <Yurii_Honchar@epam.com>
2015-12-08 19:52:38 +0200

    first step

---
+++ GameStore/GameStore.Infrastructure/Enums/NotificationMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Infrastructure.Enums
+{
+    public enum NotificationMethod
+    {
+        Email = 1,
+        Sms = 2,
+        MobileApp = 3
+    }
+}
